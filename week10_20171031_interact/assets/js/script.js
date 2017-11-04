47fd340f3687202a97abf04378234083f56eb823
doublechou <30583873+doublechou@users.noreply.github.com>
2017-11-04 16:29:14 -0400

    a

--- week10_20171031_interact/assets/js/script.js
+++ week10_20171031_interact/assets/js/script.js
@@ -1,110 +1,85 @@
-
 var list = document.getElementById('list');
-var A = document.getElementById('A');
-
-var i = 0;
-var j = 0;
-console.log(A);
-console.log(list);
-A.addEventListener("click", refreshMap);
-
-// runs whole function as soon as page opens. We wrap all our code in this function.
-(function() {
-  //Set Event listener on Button. When clicked, run the makeRequest function defined below
-  document.getElementById('findCity').addEventListener("click", makeRequest);
-  document.getElementById('findCity').addEventListener("click", refreshMap);
-  //variable to hold httpRequest info
-  var httpRequest;
-  //makeRequest function runs when button is clicked
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-    //If something is wrong with request, pop-up an alert that says so
-    if (!httpRequest) {
-      alert("It did not work :(");
-      return false;
-    }
-    httpRequest.onreadystatechange = fillInfo;
-    httpRequest.open(
-      "GET", "https://api.openaq.org/v1/cities"
-    );
-    httpRequest.send();
-  }
-
-  //Function that Runs When API Call is finished
-  function fillInfo() {
-    //variable to hold the response from API GET request
-    var responseContent;
-    var listArray = [];
-
-   // document.write("10 cities is as below:");
-    console.log("5 cities is as below:");
-    
-
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-          console.log(httpRequest.responseText);
-
-          console.log("@@@@@");
-        
-          responseContent = JSON.parse(httpRequest.responseText);
-
-          for (i = j;i<j+5;i++) {
-
-            console.log(responseContent.results[i].city);
-    
-            var listItem = document.createElement('li');
-            listItem.appendChild(document.createTextNode(responseContent.results[i].city));
-            list.appendChild(listItem);
-          }
-          j+=5;
-      } else {
-        alert("There was a problem with the request.");
-      }
-    }
-  }
-  //Make sure you ad the () after the end of the function.
-})();
+var beijing={lat:39.9042, lng:116.4074};
+var paris={lat:48.8566, lng:2.3522};
+var uluru = {lat: -25.363, lng: 131.044};
+var zoom = 10;
+var zoomInit=2;
+$(document).ready(function(){
 
 
-function initMap() {
-        var paris={lat:48.8566, lng:2.3522};
-        var uluru = {lat: -25.363, lng: 131.044};
+function initmap(zoom,center,){
+     var map  = new google.maps.Map(document.getElementById('map'), {
+          zoom: zoom,
+          center: center
+        });
 
-      
+
+}
+
+function initMap(zoom, center, marker1, marker2, marker3) {
+       
         var map  = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: paris
+          zoom: zoom,
+          center: center
         });
         var markerParis = new google.maps.Marker({
-          position: paris,
+          position: marker1,
           map: map
         });
 
         var markerUluru = new google.maps.Marker({
-          position: uluru,
+          position: marker2,
           map: map
         });
 
+        var markerBeijing = new google.maps.Marker({
+          position: marker3,
+          map: map
+        });
 
-        if (find) {
-          console.log("oooooooo");
-        }
       }
 
-
-
-
-
-function refreshMap(){
-  var beijing={lat:39.9042, lng:116.4074};
-  var map = new google.map.Map(document.getElementById('map'),{
-    zoom:4,
-    center:beijing
-  });
-  var markerBeijing = new google.maps.Marker({
-    position: beijing,
-    map:map
-  });
-
-  console.log("@@@@@@");
+      function init() {
+  //runs first
+  initmap(zoomInit, beijing);
 }
+
+function app() {
+  //thing 2
+  //thing 3
+}
+
+init();
+app();
+
+$('#searchCity').click(function(){
+  var baseURL = "https://api.openaq.org/v1/cities";
+  $.get(baseURL, function(data,status) {
+
+    if (status == "success") {
+      console.log(data);
+      //figure out stuff from the data
+      initMap(zoomInit, beijing, paris, uluru, beijing);
+    }
+  });
+
+});
+
+
+
+$('#zoom_up').click(function(){
+ 
+      initMap(zoom, beijing, paris, uluru, beijing);
+      zoom+=4;
+ 
+});
+
+
+
+
+
+
+
+
+});
+
